f9179d93ae12bf031a748890a867b67ef6eb9fd5
Diggory Blake <diggsey@googlemail.com>
2020-04-27 17:00:30 +0100

    Add config and metadata tests

---
+++ tests/docfetch/test_config.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+from email.utils import formatdate
+
+
+def test_config_protected(session, auth):
+    # Should require authentication
+    r = session.get('http://app/docfetch/config')
+    assert r.status_code == 401
+
+    # Should require correct key
+    bad_key = os.urandom(256)
+    r = session.get('http://app/docfetch/config', auth=auth(key=bad_key))
+    assert r.status_code == 401
+
+    # Should require '(request-target)' *and* 'date' headers to be signed
+    r = session.get('http://app/docfetch/config', auth=auth(headers=['date']))
+    assert r.status_code == 401
+
+    # Should require 'date' header to be recent
+    old_date = formatdate(time.time() - 120)
+    r = session.get('http://app/docfetch/config', headers={'date': old_date}, auth=auth())
+    assert r.status_code == 401
+
+
+def test_config_smoke(session, auth):
+    r = session.get('http://app/docfetch/config', auth=auth())
+    assert r.status_code == 200
+    assert r.headers['content-type'] == 'application/json'
+
+    res = r.json()
+
+    assert 'check_type' in res
+    assert 'check_template' in res
+
---
+++ tests/docfetch/test_metadata.py
@@ -0,0 +1,8 @@
+def test_metadata(session):
+    r = session.get('http://app/docfetch/')
+    assert r.status_code == 200
+    assert r.headers['content-type'] == 'application/json'
+
+    res = r.json()
+    assert res['protocol_version'] == 1
+    assert isinstance(res['provider_name'], str)
