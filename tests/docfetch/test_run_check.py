8411733bde32d54d39694accaeb61f5e528c9eca
Michael Holmes <michael.holmes@passfort.com>
2021-07-14 14:21:26 +0100

    Fix and improve test for integration file downloads

--- tests/docfetch/test_run_check.py
+++ tests/docfetch/test_run_check.py
@@ -101,7 +101,7 @@ def test_retrieve_demo_from_finish_endpoint(cbmock, session, auth):
     assert complete_result['check_output']['documents'][0]['verification_result']['all_passed']
 
 @patch('app.docfetch.task_thread')
-def test_download_image(cbmock, session, auth):
+def test_download_files(cbmock, session, auth):
     check_id = str(uuid4())
     provider_config = {
         'require_dob': False,
@@ -161,18 +161,67 @@ def test_download_image(cbmock, session, auth):
     assert documents[0]['verification_result']['all_passed']
     assert len(documents[0]['images']) > 0
     image_reference = documents[0]['images'][0]['provider_reference']
+    assert len(documents[0]['files']) == 2
 
     # Download the image
-    complete_request = session.post('http://app/docfetch/download_image', json={
+    download_image_request = session.post('http://app/docfetch/download_file', json={
         'check_id': check_id,
-        'image_reference': image_reference,
+        'file_reference': image_reference,
+        'download_info': {
+            'download_type': 'IMAGE',
+            'image_type': documents[0]['images'][0]['image_type']
+        },
         'commercial_relationship': 'DIRECT',
         'provider_config': provider_config,
         'custom_data': initial_result['custom_data']
     }, auth=auth())
-    assert complete_request.status_code == 200
-    assert complete_request.headers['content-type'] == 'image/png'
+    assert download_image_request.status_code == 200
+    assert download_image_request.headers['content-type'] == 'image/png'
     
-    image_data = complete_request.content
+    image_data = download_image_request.content
 
     assert len(image_data) > 0
+
+    # Download the live video
+    print(f"{documents[0]['files']}")
+    live_video = next(filter(lambda f: f['type'] == 'LIVE_VIDEO', documents[0]['files']))
+    assert live_video is not None
+    download_video_request = session.post('http://app/docfetch/download_file', json={
+        'check_id': check_id,
+        'file_reference': live_video['reference'],
+        'download_info': {
+            'download_type': 'FILE',
+            'file_type': 'LIVE_VIDEO'
+        },
+        'commercial_relationship': 'DIRECT',
+        'provider_config': provider_config,
+        'custom_data': initial_result['custom_data']
+    }, auth=auth())
+    assert download_video_request.status_code == 200
+    assert download_video_request.headers['content-type'] == 'video/mp4'
+
+    video_data = download_video_request.content
+
+    assert len(video_data) > 0
+
+    # Download the video frame
+    print(f"{documents[0]['files']}")
+    video_frame = next(filter(lambda f: f['type'] == 'VIDEO_FRAME', documents[0]['files']))
+    assert video_frame is not None
+    download_frame_request = session.post('http://app/docfetch/download_file', json={
+        'check_id': check_id,
+        'file_reference': video_frame['reference'],
+        'download_info': {
+            'download_type': 'FILE',
+            'file_type': 'VIDEO_FRAME'
+        },
+        'commercial_relationship': 'DIRECT',
+        'provider_config': provider_config,
+        'custom_data': initial_result['custom_data']
+    }, auth=auth())
+    assert download_frame_request.status_code == 200
+    assert download_frame_request.headers['content-type'] == 'image/png'
+
+    frame_data = download_frame_request.content
+
+    assert len(frame_data) > 0
